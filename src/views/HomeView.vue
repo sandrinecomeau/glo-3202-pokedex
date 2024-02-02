61270e651c99e66936a05e5c88549a5b8f13c0fe
sandrinecomeau <84459648+sandrinecomeau@users.noreply.github.com>
2024-02-01 19:31:40 -0500

    clean - vue autogen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <p>Welcome to the Pokedex app!</p>
 </template>
