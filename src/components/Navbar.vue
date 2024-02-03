9735dcde6a5e87d1e29189a947653a46771f42da
sandrinecomeau <84459648+sandrinecomeau@users.noreply.github.com>
2024-02-03 16:04:30 -0500

    add - sass confing for bootstrap

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+<nav class="navbar bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../assets/img/pokeball.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      Pokédex
+    </a>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
