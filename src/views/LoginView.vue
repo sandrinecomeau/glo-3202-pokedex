faeafba48b5392219d62a775a32740e7d806fae3
sandrinecomeau <84459648+sandrinecomeau@users.noreply.github.com>
2024-02-04 03:13:59 -0500

    add - first test sign up

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,14 +1,34 @@
 <script setup>
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-function singUp() {
+function singUp(event) {
+  console.log(event)
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+  const auth = getAuth();
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage); //TODO: handle error and remove console.log (side channel potential risk)
+    });
+}
+
+function showSingUp() {
   document.getElementById('login-section').classList.add('hidden')
   document.getElementById('sign-up-section').classList.remove('hidden')
 }
 
-function login() {
+function showLogin() {
   document.getElementById('sign-up-section').classList.add('hidden')
   document.getElementById('login-section').classList.remove('hidden')
 }
+
+
 
 
 </script>
@@ -20,8 +40,8 @@ function login() {
     <p>Please login or sign up to continue</p>
 
     <div>
-      <button @click="login">Login</button>
-      <button @click="singUp">I'm new here - Sign up!</button>
+      <button @click="showLogin">Login</button>
+      <button @click="showSingUp">I'm new here - Sign up!</button>
     </div>
 
     <div class="visible" id="login-section">
@@ -30,6 +50,17 @@ function login() {
 
     <div class="hidden" id="sign-up-section">
       <p>Sign up form</p>
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="email" >
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" class="form-control" id="password">
+        </div>
+        <button class="btn btn-primary" @click="singUp">Submit</button>
+      </form>
     </div>
   </div>
 </template>
