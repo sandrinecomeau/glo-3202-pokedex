0ea6ab3712fe995af4bf3a38df3d60bd43106958
sandrinecomeau <84459648+sandrinecomeau@users.noreply.github.com>
2024-02-17 20:35:42 -0500

    add - gen data with functions for pokemons

--- functions/index.js
+++ functions/index.js
@@ -3,31 +3,42 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.addPokemon = functions.https.onCall(async (data, context) => {
-  if (!context.auth) {
-    throw new functions.https.HttpsError('unauthenticated', 'The function must be called while authenticated.');
+  try {
+
+    if (!context.auth) {
+      throw new functions.https.HttpsError('unauthenticated', 'The function must be called while authenticated.');
+    }
+
+    if (data.nickname.length > 100 || !data.nickname.match(/^[a-zA-Z0-9\s]*$/)) {
+      throw new functions.https.HttpsError('invalid-argument', 'Nickname must be 100 characters or less and only contain letters, numbers and spaces.');
+    }
+
+    let species = data.species;
+    species = species.toLowerCase().replace(/\s/g, '');
+    species = species.charAt(0).toUpperCase() + species.slice(1);
+    const nickname = data.nickname;
+    const user = context.auth.uid;
+
+    const supportedPokemonRef = admin.firestore().collection('supported_pokemons').doc(species);
+    const doc = await supportedPokemonRef.get();
+
+    if (!doc.exists) {
+      throw new functions.https.HttpsError('not-found', species + ': The specified Pokémon species is not supported.');
+    } else {
+      const newPokemon = {
+          species,
+         nickname,
+         user,
+         dex: doc.data().dex,
+         type: doc.data().type,
+         img: doc.data().id,
+       };
+
+      //await db.collection('pokemon').add(newPokemon) then return the id
+      await admin.firestore().collection('pokemon').add(newPokemon);
+      return {message: `Pokemon ${species} successfully added.`};
+    }
+  } catch (error) {
+      throw new functions.https.HttpsError('internal', error.message);
   }
-
-  const species = data.species;
-  const nickname = data.nickname;
-  const user = context.auth.uid;
-
-  const supportedPokemonRef = admin.firestore().collection('supported_pokemon').doc(species);
-  const doc = await supportedPokemonRef.get();
-
-  if (!doc.exists) {
-    throw new functions.https.HttpsError('not-found', 'The specified Pokémon species is not supported.');
-  }
-
-  const newPokemon = {
-    species,
-    nickname,
-    user,
-    dex: doc.data().dex,
-    type: doc.data().type,
-    img: doc.data().img,
-  };
-
-  await admin.firestore().collection('pokemons').add(newPokemon);
-
-  return { message: `Pokemon ${species} successfully added.` };
 });
